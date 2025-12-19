cf19d682fbefb80a62bc64386c9d02219c252924
Jagger <jyung888@terpmail.umd.edu>
2025-12-19 05:51:32 -0500

    feat: Adding feature to add customers

--- public/customers.js
+++ public/customers.js
@@ -1,8 +1,25 @@
+async function createCustomer() {
+    await fetch(`/customer`, {
+        method: 'POST',
+        body: JSON.stringify({
+            firstName: `${document.getElementById('firstName').value}`,
+            lastName: `${document.getElementById('lastName').value}`,
+            state: `${document.getElementById('state').value}`,
+        }),
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    }).then((result) => result.json());
+
+    await loadCustomerData();
+};
+
 async function loadCustomerData() {
     await fetch("/customers")
     .then((result) => result.json())
     .then((resultJson) => {
         const table = document.createElement('table');
+        table.setAttribute('id', 'customerInfo')
         const tableRow = document.createElement('tr');
 
         const tableHeadingFirstName = document.createElement('th');
@@ -38,6 +55,11 @@ async function loadCustomerData() {
 
             table.appendChild(customerTableRow);
         });
+
+        const preExistingTable = document.getElementById('customerInfo')
+        if(preExistingTable) {
+            preExistingTable.remove();
+        }
 
         document.body.appendChild(table);
     });
